17158eb8084639d7c6768659839226e6135cfcb4
buiquanghai203 <163149696+buiquanghai203@users.noreply.github.com>
2025-03-31 20:50:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,7 @@ def main():
         cols_reordered = cols[:10] + cols[30:] + cols[10:30]
         # Sắp xếp lại DataFrame
         sales_merged = sales_merged[cols_reordered]
+        sales_merged = sales_merged.sort_values(by=["id"])
         st.write(sales_merged) 
         
         # Select all features except 'id', 'date', and 'sales' for model input (last 15 days)
